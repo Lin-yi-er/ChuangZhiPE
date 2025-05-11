a43b2efef12228ccdb5bf9805c46577e52c6a8df
Lyyo <142208241+Lin-yi-er@users.noreply.github.com>
2025-05-11 12:43:42 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,3 +1,3 @@
 python main.py \
---api_key sk-c28e4b3a4de94bbbb3b8b20d4f4e08a8 --base_url https://api.deepseek.com --model_name deepseek-chat \
+--api_key your_api_key --base_url https://api.deepseek.com --model_name deepseek-chat \
 --temperature 0.0 --ndcg@k 10 --is_multi_turn false
